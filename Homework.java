4cbb3c56fa283302a60288b5e58e4556e896ff77
MekhiW34 <washington.mekhi@yahoo.com>
2021-03-16 23:28:35 -0400

    first commit

---
+++ Homework.java
@@ -0,0 +1,100 @@
+package march17th;
+
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+
+        //    Question 1
+//    Scanner userInput =  new Scanner(System.in);
+//        System.out.println("Whats the temp");
+//        int temp = userInput.nextInt();
+//        if(temp > 90 ){
+//            System.out.println("Sunblock may be needed");
+//        }else if(temp >70 && temp <=80){
+//            System.out.println("may need a coat");
+//        }else{
+//            System.out.println("it muggy asf out");
+//        }
+
+
+//    Question 2
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Enter month's number: ");
+//        int monthNumber;
+//        monthNumber = in.nextInt();
+//        switch (monthNumber) {
+//            case 1:
+//                System.out.println("January");
+//                break;
+//            case 2:
+//                System.out.println("February");
+//                break;
+//            case 3:
+//                System.out.println("March");
+//                break;
+//            case 4:
+//                System.out.println("April");
+//                break;
+//            case 5:
+//                System.out.println("May");
+//                break;
+//            case 6:
+//                System.out.println("June");
+//                break;
+//            case 7:
+//                System.out.println("July");
+//                break;
+//            case 8:
+//                System.out.println("August");
+//                break;
+//            case 9:
+//                System.out.println("September");
+//                break;
+//            case 10:
+//                System.out.println("October");
+//                break;
+//            case 11:
+//                System.out.println("November");
+//                break;
+//            case 12:
+//                System.out.println("December");
+//                break;
+//            default:
+//                System.out.println("Invalid month.");
+//                break;
+//        }
+
+//        Question 3
+//        String firstName = "Christopher";
+//        System.out.println(firstName.substring(0,5));
+//
+
+
+
+
+
+//        Question 4
+//        int x = 10;
+//        while(x >0){
+//            System.out.println(x);
+//            x--;
+
+//            for(int x=10; x>0; x--) {
+//                System.out.println(x);
+//            }
+//
+//        Question 5
+//
+//        Scanner userInput = new Scanner(System.in);
+//        System.out.println("Whats the number?");
+//        int age = userInput.nextInt();
+//        if(age > 21 ){
+//            System.out.println("Welcome Mekhi");
+//        }else{
+//            System.out.println("Try Again");
+//        }
+
+
+    }
+}
